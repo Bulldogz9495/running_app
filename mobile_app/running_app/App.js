4909816a24ea81e40bf609110428b4bd8b1b48a0
Bulldogz9495 <41877597+Bulldogz9495@users.noreply.github.com>
2024-03-28 21:46:32 -0400

    Added a login screen that should connect to auth api. Need to deploy a container before it will work.

--- mobile_app/running_app/App.js
+++ mobile_app/running_app/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider as PaperProvider } from 'react-native-paper';
 
+import LoginScreen from './screens/LoginScreen';
 import TabNavigation from './navigation/TabNavigation';
 import { UserProvider } from './navigation/userContext';
 
+const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -13,7 +16,10 @@ export default function App() {
     <PaperProvider>
       <UserProvider>
         <NavigationContainer>
-          <TabNavigation />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="main" component={TabNavigation} />
+          </Stack.Navigator>
         </NavigationContainer>
       </UserProvider>
     </PaperProvider>
