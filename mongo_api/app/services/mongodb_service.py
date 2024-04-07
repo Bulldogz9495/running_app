f31ba22fb1f589c255a9edd7d218110d9fb93836
Bulldogz9495 <41877597+Bulldogz9495@users.noreply.github.com>
2024-04-07 15:53:03 -0400

    Improve some env settings and set up aws env file - Mongo db now exists in prod

--- mongo_api/app/services/mongodb_service.py
+++ mongo_api/app/services/mongodb_service.py
@@ -1,13 +1,17 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from app.settings import DATABASE_URL, DATABASE_NAME, logger, DBPORT, DBHOST
+from app.settings import DATABASE_URL, DATABASE_NAME, logger, DBPORT, DBHOST, ENVIRONMENT
 import pymongo
 
 
 class MongoDBService:
     def __init__(self):
-        self.sync_client = pymongo.MongoClient(DBHOST, int(DBPORT))
+        if ENVIRONMENT == 'local':
+            self.sync_client = pymongo.MongoClient(DBHOST, int(DBPORT))
+            self.client = AsyncIOMotorClient(DBHOST, int(DBPORT))
+        else:
+            self.sync_client = pymongo.MongoClient(DATABASE_URL)
+            self.client = AsyncIOMotorClient(DATABASE_URL)
         self.sync_db = self.sync_client[DATABASE_NAME]
-        self.client = AsyncIOMotorClient(DBHOST, int(DBPORT))
         self.db = self.client[DATABASE_NAME]
 
     
