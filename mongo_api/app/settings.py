b9000967f25d1a2598f40fdcd1617804986d67e1
Bulldogz9495 <41877597+Bulldogz9495@users.noreply.github.com>
2024-04-08 16:54:19 -0400

    Add a pymongo aws login

--- mongo_api/app/settings.py
+++ mongo_api/app/settings.py
@@ -23,7 +23,10 @@ while "DATABASE_USER" not in os.environ:
 DBHOST = os.environ.get('DBHOST', default="mongodb")
 DBPORT = os.environ.get('DBPORT', default=27017)
 ENVIRONMENT= os.environ.get('ENVIRONMENT')
-DATABASE_URL = f"""mongodb+srv://{os.environ.get("DATABASE_USER")}:{os.environ.get("DATABASE_PASSWORD")}@cluster0.dku630t.mongodb.net/"""
+if ENVIRONMENT == "local":
+    DATABASE_URL = f"""mongodb+srv://{os.environ.get("DATABASE_USER")}:{os.environ.get("DATABASE_PASSWORD")}@cluster0.dku630t.mongodb.net/"""
+else:
+    DATABASE_URL = f"""mongodb+srv://{os.environ.get("DATABASE_USER")}:{os.environ.get("DATABASE_PASSWORD")}@cluster0.dku630t.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&appName=Cluster0"""
 DATABASE_NAME = "running_data"
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 LOCALDEV = True
