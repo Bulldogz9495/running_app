ac6769068293b038fcdeee03d7cf8652847f39e0
Bulldogz9495 <41877597+Bulldogz9495@users.noreply.github.com>
2024-04-09 22:02:23 -0400

    Add session token to mongo db connection

--- mongo_api/app/settings.py
+++ mongo_api/app/settings.py
@@ -1,6 +1,7 @@
 import os, time
 import logging
 import dotenv
+import boto3
 
 
 def setup_logger():
@@ -26,7 +27,13 @@ ENVIRONMENT= os.environ.get('ENVIRONMENT')
 if ENVIRONMENT == "local":
     DATABASE_URL = f"""mongodb+srv://{os.environ.get("DATABASE_USER")}:{os.environ.get("DATABASE_PASSWORD")}@cluster0.dku630t.mongodb.net/"""
 else:
-    DATABASE_URL = f"""mongodb+srv://{os.environ.get("DATABASE_USER")}:{os.environ.get("DATABASE_PASSWORD")}@serverlessinstancechall.ztcznqz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&appName=ServerlessInstanceChallengeRun"""
+    sts_client = boto3.client('sts')
+    assumed_role_object=sts_client.assume_role(
+        RoleArn=os.environ.get("ROLE_ARN"),
+        RoleSessionName="AssumeRoleSession1"
+    )
+    credentials=assumed_role_object['Credentials']
+    DATABASE_URL = f"""mongodb+srv://{credentials['AccessKeyId']}:{credentials['SecretAccessKey']}@serverlessinstancechall.ztcznqz.mongodb.net/?authSource=%24external&authMechanism=MONGODB-AWS&retryWrites=true&w=majority&authMechanismProperties=AWS_SESSION_TOKEN:{credentials['SessionToken']}&appName=ServerlessInstanceChallengeRun
 DATABASE_NAME = "running_data"
 JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
 LOCALDEV = True
